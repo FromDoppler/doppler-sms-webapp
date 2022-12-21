ccf5086a835b53f08d9a41d5fb5786c5a308f204
mcappato <mcappato@makingsense.com>
2022-12-21 17:07:52 -0300

    chore: module pagination component

---
+++ src/custom-components/Pagination/Pagination.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { usePagination, DOTS } from "./usePagination";
+
+export interface PaginationInterface {
+  totalCount: number;
+  pageSize: number;
+  currentPage: number;
+  onPageChange: (arg: number) => void;
+}
+
+const Pagination: React.FC<PaginationInterface> = ({
+  totalCount,
+  pageSize,
+  currentPage,
+  onPageChange,
+}) => {
+  const { paginationRange } = usePagination({
+    totalCount,
+    pageSize,
+    currentPage,
+  });
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <>
+      <nav className="dp-pagination">
+        <ul>
+          <li>
+            <button
+              type="button"
+              className="ms-icon icon-arrow-prev"
+              onClick={onPrevious}
+              disabled={currentPage === 1}
+            ></button>
+          </li>
+          {paginationRange.map((pageNumber, index) => {
+            if (pageNumber === DOTS) {
+              return (
+                <li key={index}>
+                  <button className="dp-pag-point"></button>
+                </li>
+              );
+            }
+            if (pageNumber === currentPage)
+              return (
+                <li key={index}>
+                  <span className="dp-active-page" aria-current="page">
+                    {pageNumber}
+                  </span>
+                </li>
+              );
+            return (
+              <li key={index}>
+                <button type="button" onClick={() => onPageChange(pageNumber)}>
+                  {pageNumber}
+                </button>
+              </li>
+            );
+          })}
+          <li>
+            <button
+              type="button"
+              className="ms-icon icon-arrow-next"
+              onClick={onNext}
+              disabled={currentPage === lastPage}
+            ></button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Pagination;
